7ff76111179ba912c58c3d0beb564e883afd1bca
Charlie Gibson <cgibson1619@gmail.com>
2017-07-06 18:36:21 -0500

    Add Relationship model and table, set up relationships between models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  # user model
   has_many :tweets
+  has_many :relationships
+  has_many :friends, through: :relationships
+
+  # friend model
+  has_many :inverse_relationships, class_name: 'Relationship', foreign_key: 'friend_id'
+  has_many :inverse_friends, through: :inverse_relationships, source: :user
 
   validates :username, presence: true, uniqueness: true
 end
